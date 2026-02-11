a5977f66af344d5fae87c542cd0395cab84b517b
Rationess <epsilonrationes@gmail.com>
2026-02-11 15:07:47 +0300

    __init__

--- aioitd/__init__.py
+++ aioitd/__init__.py
@@ -1,4 +1,11 @@
 from aioitd.client import AsyncITDClient, is_token_expired, validate_limit, verify_password, datetime_to_itd_format, \
     valid_file_mimetype, valid_hashtag_name, FetchInterval
 from aioitd.exceptions import *
-from aioitd import models
+from aioitd.models import Annotated, AudioCommentAttachment, Author, AuthorWithoutId, BaseAuthor, BaseComment, \
+    BaseFullUser, BaseModel, BasePost, BasePostWithoutAuthorId, BeforeValidator, BlockedAuthor, BlockedUser, Clan, \
+    Comment, CommentPagination, ConfigDict, Counts, Field, File, FollowPagination, FollowUser, FullMe, FullPost, \
+    FullUser, GetFile, Hashtag, ITDBaseModel, ITDDatetime, ImageCommentAttachment, ImagePostAttachment, \
+    IntCommentPagination, IntPagination, InvalidPostAttachment, Me, Notification, OriginalPost, Pagination, \
+    Pin, PinWithDate, PopularPost, Post, PostWithoutAuthorId, Privacy, Profile, Reply, ReplyComment, Report, Span, \
+    TimePagination, UUIDCommentPagination, UUIDPagination, UpdatePostResponse, User, UserBlockMe, UserPost, \
+    UserPostWithoutAuthorId, UserWithRole, VerifiedUser, VideoCommentAttachment, WallRecipient
