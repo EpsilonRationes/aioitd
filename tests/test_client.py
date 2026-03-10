9957e593a28db72f535c1a62d3fb466519cbcd7d
Rationess <epsilonrationes@gmail.com>
2026-03-10 08:12:40 +0300

    tests: add test_get_me_uuid, test_is_token_expired

--- tests/test_client.py
+++ tests/test_client.py
@@ -20,3 +20,16 @@ async def test_client():
             async for event in events:
                 print(event)
                 break
+
+
+@pytest.mark.asyncio
+async def test_get_me_uuid():
+    async with AsyncITDClient(refresh_token) as client:
+        await client.get_me_uuid()
+
+@pytest.mark.asyncio
+async def test_is_token_expired():
+    async with AsyncITDClient(refresh_token) as client:
+        await client.get_me()
+        client._access_token = "eyJhbGciOiJFZERTQSIsImtpZCI6Imw3azBzcUJSYWVmRVE2V2JiWkhPWVNiZV9SWDRIWDh1MkxXWV9qVnJWVHciLCJ0eXAiOiJKV1QifQ.eyJyb2xlcyI6WyJ1c2VyIl0sInNpZCI6IjVhZGNjM2Y0LWRhNjUtNDdkMC1hMDc2LTQyZDc3OWIwNDM5ZSIsImlzQWN0aXZlIjp0cnVlLCJzdWIiOiIzMzBkZWEyMC1iYjdjLTRjOTYtYWQwOS05NzE1MGYxYWQ1ZjYiLCJpYXQiOjE3NzMxMTg0MjcsImlzcyI6ImF1dGgtc2VydmljZSIsImV4cCI6MTc3MzExOTMyNywianRpIjoiZmRjMWU5MmEtYTBhZS00MGNiLThhZmUtMjFkMDJjYmZmY2EwIn0.uXn13PW1DjpYtBfmzNnhLeBLHx2yC3jgVamENcvE0FtU1ReMtSvg4xuvm1L-N5x-dHKYhsSJHBqZLqvLQOCxAQ"
+        await client.get_me()
